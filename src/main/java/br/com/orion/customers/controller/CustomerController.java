159084a335645aacd1fd3f7586a1c131ddaa5256
lNotini <lucasnotini@hotmail.com>
2022-11-08 22:16:30 -0300

    adiciona pagina inserir cliente

--- src/main/java/br/com/orion/customers/controller/CustomerController.java
+++ src/main/java/br/com/orion/customers/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package br.com.orion.customers.controller;
 
+import br.com.orion.customers.domain.Customer;
 import br.com.orion.customers.domain.CustomerRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -19,6 +22,19 @@ public class CustomerController {
     public String customers(Model model){ //MÉTODO LISTAR CLIENTES
         model.addAttribute("customersList", customerRepo.findAll());
         return "customers/index";
+    }
+
+    //crio método para atender requisição de quando clicar no botão inserir, abrindo página html de salvar/editar
+    @GetMapping("/customers/new")
+    public String newCustomer(@ModelAttribute("customer") Customer customer){
+        return "customers/form";
+    }
+
+    //crio método para salvar pessoa
+    @PostMapping("/customers/salvar")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer){
+        customerRepo.save(customer);
+        return "redirect:/customers";
     }
 
 }
